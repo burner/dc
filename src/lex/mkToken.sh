e05bfa0cf1617851a9e488478b21a1f37036bb15
Robert BuRnEr Schadek <realburner@gmx.de>
2010-10-25 21:32:05 +0200

    nice script to create token dumps

---
+++ src/lex/mkToken.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for line in $(cat mktoc)
+do
+	fn=`echo ${line} | tr A-Z a-z`
+	f=$fn".d"
+	echo -e "module lex.$fn;\n\nimport lex.token;\n\nclass $line : Token {\n\tpublic static final dstring NAME = \"$line\";\n\n\tthis(Token prev) {\n\t\tsuper(prev);\n\t}\n}" > $f
+	#rm $f
+done
