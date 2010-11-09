8458b8353881741b2d0fbf7466cec1e304d453be
Robert BuRnEr Schadek <rburners@gmail.com>
2010-11-09 12:09:02 +0100

    not needed anymore

--- src/lex/mkToken.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for line in $(cat mktoc)
-do
-	fn=`echo ${line} | tr A-Z a-z`
-	f=$fn".d"
-	echo -e "module lex.$fn;\n\nimport lex.token;\n\nclass $line : Token {\n\tpublic static final dstring NAME = \"$line\";\n\n\tthis(Token prev) {\n\t\tsuper(prev);\n\t}\n}" > $f
-	rm $f
-done
